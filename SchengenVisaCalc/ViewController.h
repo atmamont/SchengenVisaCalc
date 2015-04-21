452b0260509310f7d2e029c820d72d6e1e5162db
atMamont <atMamont@Mamont-Mac.local>
2015-04-21 23:12:08 +0300

    Initial Commit

---
+++ SchengenVisaCalc/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  SchengenVisaCalc
+//
+//  Created by atMamont on 21.04.15.
+//  Copyright (c) 2015 Andrey Mamchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ SchengenVisaCalc/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  SchengenVisaCalc
+//
+//  Created by atMamont on 21.04.15.
+//  Copyright (c) 2015 Andrey Mamchenko. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
